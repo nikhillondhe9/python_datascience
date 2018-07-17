aefd4457b142c058d540bb2ad289b25254f1976c
nikhillondhe9 <nikhillondhe9@gmail.com>
2018-07-17 17:46:22 -0400

    Matplotlib initial commit

---
+++ Matplotlib/app.py
@@ -0,0 +1,47 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class MyPlot(object):
+
+    def __init__(self):
+        self.x = np.arange(0, 100)
+        self.y = self.x*2
+        self.z = self.x ** 2
+        self.fig = plt.figure()
+
+    def plot1(self):
+        ax = self.fig.add_axes([0, 0, 1, 1])
+        ax.set_title('Plot 1')
+        ax.set_xlabel('X axis')
+        ax.set_ylabel('Y axis')
+        ax.plot(self.x, self.y)
+        plt.tight_layout()
+        plt.show()
+
+    def internal_plot(self):
+        ax1 = self.fig.add_axes([0, 0, 1, 1])
+        ax2 = self.fig.add_axes([0.2, 0.5, 0.2, 0.2])
+        ax1.set_title('External Plot ')
+        ax1.set_xlabel('X axis')
+        ax1.set_ylabel('Y axis')
+        ax2.set_title('Internal Plot ')
+        ax2.set_xlabel('X axis')
+        ax2.set_ylabel('Y axis')
+        ax1.plot(self.x, self.y)
+        ax2.plot(self.x, self.y)
+        plt.tight_layout()
+        plt.show()
+
+    def sub_plots(self):
+        fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(8, 2))
+        ax[0].plot(self.x, self.y, lw=3, ls='--', color='red')
+        ax[1].plot(self.x, self.z, lw=3, ls='--', color='blue')
+        plt.show()
+
+
+if __name__ == '__main__':
+    myplot = MyPlot()
+    myplot.internal_plot()
+    myplot.plot1()
+    myplot.sub_plots()
